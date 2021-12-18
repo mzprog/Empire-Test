c96448916e791a6ce2a10a6197e1a5d74c22b0ab
mohamad zbib <mz32programmer@gmail.com>
2021-12-19 00:31:46 +0200

    create simple `LoadingPage`

---
+++ front_code/Components/Layouts/LoadingPage.js
@@ -0,0 +1,10 @@
+export default () => {
+    return (
+        <div className="bg-sky-200">
+            <div className="h-screen flex justify-center items-center 
+            text-4xl font-bold text-sky-600 animate-pulse">
+                Loading...
+            </div>
+        </div>
+    )
+}
