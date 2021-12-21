a2fc29efb088761be9c3e5afad2ebdb9e831fa28
Mohamad Zbib <mz32programmer@gmail.com>
2021-12-21 11:20:23 +0200

    add header

--- front_code/pages/index.js
+++ front_code/pages/index.js
@@ -1,16 +1,30 @@
+import Link from "next/link";
 import CitySearch from "../Components/Forms/CitySearch";
+import Logout from "../Components/Forms/Logout";
 import MainLayout from "../Components/Layouts/MainLayout";
 
 export default function Home() {
     return (
         <MainLayout>
-            <div className="h-screen bg-gradient-to-b from-sky-200 to-yellow-100 grid items-center">
-                <div className="container mx-auto">
-                    <div className="text-center">
-                        <CitySearch />
-                    </div>
-                    
+            <div className="flex flex-col min-h-screen">
+                <div className="bg-sky-300/70 shadow-xl border border-b-sky-300 shadow-sky-200 py-2 px-4">
+                    <Link href="/">
+                        <a className="font-bold text-white bg-black/20 p-4">
+                            Empire Test
+                        </a>
+                    </Link>
+                    <Logout />
                 </div>
+                <div className="flex-1 bg-gradient-to-b from-sky-200 to-yellow-100 grid items-center">
+                    <div className="container mx-auto">
+                        <div className="text-center">
+                            <CitySearch />
+                        </div>
+                        
+                    </div>
+                </div>
+
+
             </div>
         </MainLayout>
     )
